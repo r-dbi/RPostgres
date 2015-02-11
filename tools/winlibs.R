af60add719b45430cff8adff1f331459052a440d
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-11 00:02:57 -0800

    Bump windows build to 9.4.1

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libpq
-if(!file.exists("../windows/libpq-9.4.0/include/libpq-fe.h")){
+if(!file.exists("../windows/libpq-9.4.1/include/libpq-fe.h")){
   setInternet2()
-  curl::curl_download("https://github.com/rwinlib/libpq/archive/v9.4.0.zip", "lib.zip", quiet = TRUE, mode = "wb")
+  curl::curl_download("https://github.com/rwinlib/libpq/archive/v9.4.1.zip", "lib.zip", quiet = TRUE, mode = "wb")
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
