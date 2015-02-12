c07d6a98389f84835e797aab827b902e5e250e8e
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-11 16:27:42 -0800

    Add copy of libpq for windows to the repo

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,8 @@
 # Build against static libpq
 if(!file.exists("../windows/libpq-9.4.1/include/libpq-fe.h")){
+  # internet2.dll supports https
   setInternet2()
-  curl::curl_download("https://github.com/rwinlib/libpq/archive/v9.4.1.zip", "lib.zip", quiet = TRUE, mode = "wb")
+  download.file("https://github.com/rwinlib/libpq/archive/v9.4.1.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
