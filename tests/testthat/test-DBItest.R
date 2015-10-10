863e2e9037ad77ccf87f866462e22698a0e53ca2
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-10 20:50:54 +0200

    link to issues

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,2 +1,9 @@
 DBItest::make_context(Postgres(), NULL)
-DBItest::test_all(c("package_dependencies", "data_type", "show", "get_info", "data_null", "data_64_bit"))
+DBItest::test_all(c(
+  "package_dependencies", # #47
+  "data_type",            # #48
+  "show",                 # #49
+  "get_info",             # to be discussed
+  "data_null",            # #50
+  "data_64_bit"           # #51
+))
