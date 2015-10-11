393163e300f05a86476f745800bcb9a5358d21b8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-11 09:58:32 +0200

    add ignores for time and timestamp

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,12 +1,15 @@
 DBItest::make_context(Postgres(), NULL)
 DBItest::test_all(c(
-  "package_dependencies", # #47
-  "data_type",            # #48
-  "show",                 # #49
-  "get_info",             # to be discussed
-  "data_null",            # #50
-  "data_64_bit",          # #51
-  "data_character",       # #50
-  "data_date",            # #52
+  "package_dependencies",  # #47
+  "data_type",             # #48
+  "show",                  # #49
+  "get_info",              # to be discussed
+  "data_null",             # #50
+  "data_64_bit",           # #51
+  "data_character",        # #50
+  "data_date",             # #52
+  "data_time_parens",      # syntax not supported
+  "data_timestamp",        # #53
+  "data_timestamp_parens", # syntax not supported
   NULL
 ))
