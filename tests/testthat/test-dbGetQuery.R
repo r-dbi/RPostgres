5c47bd6d33339f40429aaa520bde926c078c8979
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-14 15:46:59 +0100

    DBItest: data_character and roundtrip_character

--- tests/testthat/test-dbGetQuery.R
+++ tests/testthat/test-dbGetQuery.R
@@ -1,19 +1,5 @@
 context("dbGetQuery")
 
-test_that("special charaters work", {
-  angstrom <- enc2utf8("\\u00e5")
-
-  con <- dbConnect(RPostgres::Postgres())
-
-  dbGetQuery(con, "CREATE TEMPORARY TABLE test1 (x TEXT)")
-  dbGetQuery(con, "INSERT INTO test1 VALUES ('\\u00e5')")
-
-  expect_equal(dbGetQuery(con, "SELECT * FROM test1")$x, angstrom)
-  expect_equal(dbGetQuery(con, "SELECT * FROM test1 WHERE x = '\\u00e5'")$x,
-    angstrom)
-})
-
-
 test_that("JSONB format is recognized", {
 
   con <- dbConnect(RPostgres::Postgres())
