af6fe3e42a342674ec5a2307a6a8ed732ec87f24
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-06-08 18:40:38 +0200

    ignore test that doesn't pass yet

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -57,6 +57,7 @@ DBItest::test_all(c(
   "bind_logical_int_positional_dollar",         # not an error, logicals supported natively
   "bind_raw_positional_dollar",                 # 66
   "bind_null_positional_dollar",                # 67
+  "bind_repeated_positional_dollar",            # 87
   "bind_timestamp_positional_dollar",           # 53
   "bind_timestamp_lt_positional_dollar",        # 53
 
