ec4660c10ba50c2da73ee8685ac7819f66b78859
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-06 16:41:06 +0100

    split PqUtils.cpp

--- src/PqUtils.h
+++ src/PqUtils.h
@@ -1,9 +1,6 @@
 #ifndef __RPOSTGRES_PQ_UTILS__
 #define __RPOSTGRES_PQ_UTILS__
 
-#include <Rcpp.h>
-#include <libpq-fe.h>
-
 // Generic data frame utils ----------------------------------------------------
 
 enum PGTypes {
@@ -18,43 +15,7 @@ enum PGTypes {
   PGTime
 };
 
-Rcpp::List inline dfResize(Rcpp::List df, int n) {
-  R_xlen_t p = df.size();
-
-  Rcpp::List out(p);
-  for (R_xlen_t j = 0; j < p; ++j) {
-    out[j] = Rf_lengthgets(df[j], n);
-  }
-
-  out.attr("names") = df.attr("names");
-  out.attr("class") = df.attr("class");
-  out.attr("row.names") = Rcpp::IntegerVector::create(NA_INTEGER, -n);
-
-  return out;
-}
-
-Rcpp::List inline dfCreate(const std::vector<PGTypes>& types, const std::vector<std::string>& names, int n) {
-  R_xlen_t p = types.size();
-
-  Rcpp::List out(p);
-  out.attr("names") = names;
-  out.attr("class") = "data.frame";
-  out.attr("row.names") = Rcpp::IntegerVector::create(NA_INTEGER, -n);
-
-  int j = 0;
-  for (std::vector<PGTypes>::const_iterator it = types.begin(); it != types.end(); ++it, j++) {
-    switch (*it) {
-    case PGDate:
-    case PGDatetime:
-    case PGDatetimeTZ:
-    case PGTime:
-      out[j] = Rf_allocVector(REALSXP, n);
-      break;
-    default:
-      out[j] = Rf_allocVector(static_cast<SEXPTYPE>(*it), n);
-    }
-  }
-  return out;
-}
+Rcpp::List dfResize(Rcpp::List df, int n);
+Rcpp::List dfCreate(const std::vector<PGTypes>& types, const std::vector<std::string>& names, int n);
 
 #endif
--- src/RPostgres_types.h
+++ src/RPostgres_types.h
@@ -1,6 +1,9 @@
 #ifndef __RPOSTGRES_TYPES__
 #define __RPOSTGRES_TYPES__
 
+#include <Rcpp.h>
+#include <libpq-fe.h>
+
 #include "PqConnection.h"
 #include "PqResult.h"
 
