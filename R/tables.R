6701d4aaf7f38ae6f46bc6a76f1ba70ccf87f2d0
Kirill Müller <krlmlr+r@mailbox.org>
2017-11-20 05:45:31 +0100

    fix read_table_error

--- R/tables.R
+++ R/tables.R
@@ -137,6 +137,14 @@ format_keep_na <- function(x, ...) {
 #' @rdname postgres-tables
 setMethod("dbReadTable", c("PqConnection", "character"),
   function(conn, name, ..., check.names = TRUE, row.names = FALSE) {
+    if ((!is.logical(row.names) && !is.character(row.names)) || length(row.names) != 1L)  {
+      stopc("`row.names` must be a logical scalar or a string")
+    }
+
+    if (!is.logical(check.names) || length(check.names) != 1L)  {
+      stopc("`check.names` must be a logical scalar")
+    }
+
     name <- dbQuoteIdentifier(conn, name)
     out <- dbGetQuery(conn, paste("SELECT * FROM ", name), row.names = row.names)
 
