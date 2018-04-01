4019efd0f67194d87cc3c3d953fc4ec639b1b614
Kirill Müller <krlmlr+r@mailbox.org>
2018-04-01 21:50:56 +0200

    safer tic.R

--- tic.R
+++ tic.R
@@ -1,13 +1,19 @@
 add_package_checks()
 
-get_stage("deploy") %>%
-  add_step(step_build_pkgdown())
-
-if (Sys.getenv("id_rsa") != "" && Sys.getenv("BUILD_PKGDOWN") != "") {
-  get_stage("before_deploy") %>%
-    add_step(step_install_ssh_keys()) %>%
-    add_step(step_test_ssh())
-
+if (Sys.getenv("BUILD_PKGDOWN") != "" && !ci()$is_tag()) {
   get_stage("deploy") %>%
-    add_step(step_push_deploy())
+    add_step(step_build_pkgdown())
+
+  if (Sys.getenv("id_rsa") != "") {
+    get_stage("before_deploy") %>%
+      add_step(step_setup_ssh())
+
+    # pkgdown documentation can be built optionally. Other example criteria:
+    # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+    # - `ci()$is_tag()`: Only for tags, not for branches
+    # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+    # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(commit_paths = "docs"))
+  }
 }
