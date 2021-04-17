afb194e194f1d13076464ce7645d7b6d3bb4969a
Mateusz Żółtak <zozlak@zozlak.org>
2021-04-17 09:54:50 +0200

    test-checkInterrupts.R: unneeded line removed

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -6,5 +6,4 @@ test_that("Query is executed properly when check_interrupts connection option is
   con <- postgresDefault(check_interrupts = TRUE)
   expect_equal(dbGetQuery(con, "SELECT pg_sleep(2), 'foo' AS x")$x, "foo")
   dbDisconnect(con)
-  expect_null(n)
 })
