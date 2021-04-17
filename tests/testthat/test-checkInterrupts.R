6fc784b7b2a47a3906a279a7de598160b751eb25
Mateusz Żółtak <zozlak@zozlak.org>
2021-04-17 09:59:09 +0200

    test-checkInterrupts.R: test description shortened

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -1,8 +1,6 @@
 context("checkInterrupts")
 
-test_that("Query is executed properly when check_interrupts connection option is 
-          set to TRUE and query execution takes more than check user interrupts 
-          timeout (1 second)", {
+test_that("check_interrupts = TRUE works with queries > 1 second (#244)", {
   con <- postgresDefault(check_interrupts = TRUE)
   expect_equal(dbGetQuery(con, "SELECT pg_sleep(2), 'foo' AS x")$x, "foo")
   dbDisconnect(con)
