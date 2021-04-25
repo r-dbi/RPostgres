7bb9ccbcd88874e11a00ac274c03b5a904125890
Daniel Possenriede <possenriede@gmail.com>
2021-04-25 19:42:06 +0200

    extract list tables query and make it more general

--- R/PqGenerics.R
+++ R/PqGenerics.R
@@ -33,20 +33,25 @@ setGeneric("pqListTables",
 #' @rdname pqListTables
 #' @export
 setMethod("pqListTables", "PqConnection", function(conn) {
+  query <- list_tables_sql(conn = conn)
+
+  dbGetQuery(conn, query)[["relname"]]
+})
+
+list_tables_sql <- function(conn, where_schema = NULL) {
   major_server_version <- dbGetInfo(conn)$db.version %/% 10000
 
   query <- paste0(
     # pg_class docs: https://www.postgresql.org/docs/current/catalog-pg-class.html
-    "SELECT cl.relname AS name FROM pg_class AS cl ",
-    "JOIN pg_namespace AS n ON cl.relnamespace = n.oid ",
-    "WHERE (n.nspname = ANY (current_schemas(true))) ",
-    "AND (n.nspname <> 'pg_catalog') ",
+    "SELECT n.nspname, cl.relname \n",
+    "FROM pg_class AS cl \n",
+    "JOIN pg_namespace AS n ON cl.relnamespace = n.oid \n",
     # Return only objects (relations) which the current user may access
     # https://www.postgresql.org/docs/current/functions-info.html
-    "AND (pg_has_role(cl.relowner, 'USAGE'::text) ",
-      "OR has_table_privilege(cl.oid, 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER'::text) ",
-      "OR has_any_column_privilege(cl.oid, 'SELECT, INSERT, UPDATE, REFERENCES'::text) ",
-    ") "
+    "WHERE (pg_has_role(cl.relowner, 'USAGE'::text) \n",
+    "  OR has_table_privilege(cl.oid, 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER'::text) \n",
+    "  OR has_any_column_privilege(cl.oid, 'SELECT, INSERT, UPDATE, REFERENCES'::text) \n",
+    ") \n"
   )
 
   # which kind of objects should be returned?
@@ -54,22 +59,34 @@ setMethod("pqListTables", "PqConnection", function(conn) {
     # relkind = 'p' and relispartition only supported from v10 onwards
     query <- paste0(
       query,
-      # r = ordinary table, v = view, m = materialized view, f = foreign table, p = partitioned table
-      "AND (cl.relkind IN ('r', 'v', 'm', 'f', 'p')) ",
-      "AND NOT cl.relispartition " # do not return partitions
+      # r = ordinary table, v = view, m = materialized view, f = foreign table,
+      # p = partitioned table
+      "AND (cl.relkind IN ('r', 'v', 'm', 'f', 'p')) \n",
+      "AND NOT cl.relispartition \n" # do not return partitions
     )
   } else {
     query <- paste0(
       query,
-      "AND (cl.relkind IN ('r', 'v', 'm', 'f')) "
+      "AND (cl.relkind IN ('r', 'v', 'm', 'f')) \n"
     )
+  }
+
+  if (is.null(where_schema)) {
+    # all schemas in the search path without implicitly-searched system schemas
+    # such as pg_catalog
+    query <- paste0(
+      query,
+      "AND (n.nspname = ANY (current_schemas(false))) \n"
+    )
+  } else {
+    query <- paste0(query, where_schema)
   }
 
   query <- paste0(
     query,
-    "ORDER BY name"
+    "ORDER BY cl.relkind, cl.relname"
   )
 
-  dbGetQuery(conn, query)[[1]]
-})
+  query
+}
 
