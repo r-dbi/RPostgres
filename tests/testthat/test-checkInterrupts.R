fa5cf977fcccedad0840a9f546060fb3bc0c2517
Kirill Müller <krlmlr+r@mailbox.org>
2021-09-19 11:45:02 +0200

    Skip on R 3.6

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -8,6 +8,7 @@ test_that("check_interrupts = TRUE works with queries > 1 second (#244)", {
 
 test_that("check_interrupts = TRUE interrupts immediately (#336)", {
   skip_if(Sys.getenv("R_COVR") != "")
+  skip_if(getRversion() >= "3.6" && getRversion() < "4.0")
 
   # For skipping if not available
   dbDisconnect(postgresDefault())
