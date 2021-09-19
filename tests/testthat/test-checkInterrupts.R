fffeb0fe7507cbe88c2c236ad1de9d4e64e9489a
Kirill Müller <krlmlr+r@mailbox.org>
2021-09-19 07:22:41 +0200

    Add test

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -5,3 +5,35 @@ test_that("check_interrupts = TRUE works with queries > 1 second (#244)", {
   expect_equal(dbGetQuery(con, "SELECT pg_sleep(2), 'foo' AS x")$x, "foo")
   dbDisconnect(con)
 })
+
+test_that("check_interrupts = TRUE interrupts immediately (#336)", {
+  session <- callr::r_session$new()
+
+  session$run(function() {
+    library(RPostgres)
+    .GlobalEnv$conn <- dbConnect(Postgres(), check_interrupts = TRUE)
+    invisible()
+  })
+
+  session$call(function() {
+    tryCatch(
+      print(dbGetQuery(.GlobalEnv$conn, "SELECT pg_sleep(2)")),
+      error = identity
+    )
+  })
+
+  session$poll_process(100)
+  expect_null(session$read())
+
+  session$interrupt()
+
+  # Should take much less than 1.9 seconds
+  time <- system.time(
+    expect_equal(session$poll_process(2000), "ready")
+  )
+  expect_lt(time[["elapsed"]], 1)
+
+  # Should return a proper error message
+  out <- session$read()
+  expect_match(out$result$message, "cancel")
+})
