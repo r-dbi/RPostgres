829ad8c7581f0b1b6df302cb6bf2758005b80ad0
Kirill Müller <krlmlr+r@mailbox.org>
2021-09-20 04:21:04 +0200

    Unrelated: faster test

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -2,7 +2,7 @@ context("checkInterrupts")
 
 test_that("check_interrupts = TRUE works with queries > 1 second (#244)", {
   con <- postgresDefault(check_interrupts = TRUE)
-  expect_equal(dbGetQuery(con, "SELECT pg_sleep(2), 'foo' AS x")$x, "foo")
+  expect_equal(dbGetQuery(con, "SELECT pg_sleep(0.2), 'foo' AS x")$x, "foo")
   dbDisconnect(con)
 })
 
