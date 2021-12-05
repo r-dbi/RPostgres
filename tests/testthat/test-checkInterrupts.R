d67a71a44237eb5320a0b4175b5d197b784db481
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-05 06:46:34 +0100

    Test title

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -1,6 +1,6 @@
 context("checkInterrupts")
 
-test_that("check_interrupts = TRUE works with queries > 1 second (#244)", {
+test_that("check_interrupts = TRUE works with queries < 1 second (#244)", {
   con <- postgresDefault(check_interrupts = TRUE)
   expect_equal(dbGetQuery(con, "SELECT pg_sleep(0.2), 'foo' AS x")$x, "foo")
   dbDisconnect(con)
