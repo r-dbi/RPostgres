5792b00457f45090a7fd821c093e6d8e85e6eda6
Mateusz Żółtak <zozlak@zozlak.org>
2021-12-21 17:42:33 +0100

    test-checkInterrupts.R: check if request has been cancelled sercer side
    The query interrupt test extended with a check probing if the request
    has been cancelled on the database server. The test is based on the PID
    of the connection so it should be reliable.

--- tests/testthat/test-checkInterrupts.R
+++ tests/testthat/test-checkInterrupts.R
@@ -20,6 +20,8 @@ test_that("check_interrupts = TRUE interrupts immediately (#336)", {
   session$run(function() {
     library(RPostgres)
     .GlobalEnv$conn <- postgresDefault(check_interrupts = TRUE)
+    .GlobalEnv$connPid <- dbGetQuery(.GlobalEnv$conn, "SELECT pg_backend_pid() AS pid")$pid
+    .GlobalEnv$checkConn <- postgresDefault()
     invisible()
   })
 
@@ -31,8 +33,13 @@ test_that("check_interrupts = TRUE interrupts immediately (#336)", {
   session$interrupt()
   # Interrupts are slow on Windows, give ample time
   expect_equal(session$poll_process(2000), "ready")
+  session$read()
 
-  # Tests for error behavior are brittle
+  queryStatus = session$run(function() {
+    dbGetQuery(.GlobalEnv$checkConn, "SELECT state FROM pg_stat_activity WHERE pid = $1", params = .GlobalEnv$connPid)
+  })
+  expect_equal(queryStatus$state, "idle")
+
 
   session$close()
 })
