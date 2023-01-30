a67c4c224e2e079bfc139d5886eaacd3114d20f0
antonov548 <antonov5551998@gmail.com>
2023-01-30 15:12:25 +0200

    use std::string for warning/stop

--- src/DbConnection.cpp
+++ src/DbConnection.cpp
@@ -98,14 +98,14 @@ void DbConnection::cancel_query() {
   //  * the connection is NULL or
   //  * the connection is invalid.
   PGcancel* cancel = PQgetCancel(pConn_);
-  if (cancel == NULL) cpp11::stop("Connection error detected via PQgetCancel()");
+  if (cancel == NULL) cpp11::stop(std::string("Connection error detected via PQgetCancel()"));
 
   LOG_DEBUG;
 
   // PQcancel() actually issues the cancel command to the backend.
   char errbuf[256];
   if (!PQcancel(cancel, errbuf, sizeof(errbuf))) {
-    cpp11::warning(errbuf);
+    cpp11::warning(std::string(errbuf));
   }
 
   // free up the data structure allocated by PQgetCancel().
@@ -173,7 +173,7 @@ void DbConnection::copy_data(std::string sql, cpp11::list df) {
 
 void DbConnection::check_connection() {
   if (!pConn_) {
-    cpp11::stop("Disconnected");
+    cpp11::stop(std::string("Disconnected"));
   }
 
   ConnStatusType status = PQstatus(pConn_);
@@ -268,7 +268,7 @@ void DbConnection::conn_stop(const char* msg) {
 }
 
 void DbConnection::conn_stop(PGconn* conn, const char* msg) {
-  cpp11::stop("%s: %s", msg, PQerrorMessage(conn));
+  cpp11::stop(std::string(msg) + " : " + PQerrorMessage(conn));
 }
 
 void DbConnection::cleanup_query() {
