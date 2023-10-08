2de7898829de12fecfa2674a17f69d18b913bd11
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-08 15:27:04 +0200

    Windows: update to libpq-15.3

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,9 +1,18 @@
-# Link against libpq static libraries
-VERSION <- commandArgs(TRUE)
-if(!file.exists(sprintf("../windows/libpq-%s/include/libpq-fe.h", VERSION))){
-  if(getRversion() < "3.3.0") setInternet2()
-  download.file(sprintf("https://github.com/rwinlib/libpq/archive/v%s.zip", VERSION), "lib.zip", quiet = TRUE)
+if(!file.exists("../windows/libpq/include/libpq-fe.h")){
+  unlink("../windows", recursive = TRUE)
+  url <- if(grepl("aarch", R.version$platform)){
+    "https://github.com/r-windows/bundles/releases/download/libpq-15.3/libpq-15.3-clang-aarch64.tar.xz"
+  } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
+    "https://github.com/r-windows/bundles/releases/download/libpq-15.3/libpq-15.3-clang-x86_64.tar.xz"
+  }  else if(getRversion() >= "4.2") {
+    "https://github.com/r-windows/bundles/releases/download/libpq-15.3/libpq-15.3-ucrt-x86_64.tar.xz"
+  } else {
+    "https://github.com/rwinlib/libpq/archive/v13.2.0.tar.gz"
+  }
+  download.file(url, basename(url), quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
-  unzip("lib.zip", exdir = "../windows")
-  unlink("lib.zip")
+  untar(basename(url), exdir = "../windows", tar = 'internal')
+  unlink(basename(url))
+  setwd("../windows")
+  file.rename(list.files(), 'libpq')
 }
