84c8e3b5c4c9a3e48921ebc961f7c9cfd8d22e17
Kirill Müller <kirill@cynkra.com>
2024-04-01 17:52:13 +0200

    feat!: Avoid appending a numeric suffix to duplicate column names

--- R/dbColumnInfo_PqResult.R
+++ R/dbColumnInfo_PqResult.R
@@ -3,7 +3,6 @@
 dbColumnInfo_PqResult <- function(res, ...) {
   rci <- result_column_info(res@ptr)
   rci <- cbind(rci, .typname = type_lookup(rci[[".oid"]], res@conn), stringsAsFactors = FALSE)
-  rci$name <- tidy_names(rci$name)
   rci
 }
 
