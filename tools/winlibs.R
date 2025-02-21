930452aca6db3f84c1f5fa497dfe3f7a3fdd4d0c
Jeroen Ooms <jeroenooms@gmail.com>
2025-02-21 11:09:18 +0100

    Update Windows fallback libs (again)

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,11 +1,11 @@
 if(!file.exists("../windows/libpq/include/libpq-fe.h")){
   unlink("../windows", recursive = TRUE)
   url <- if(grepl("aarch", R.version$platform)){
-    "https://github.com/r-windows/bundles/releases/download/libpq-15.11/libpq-15.11-clang-aarch64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/libpq-15.12/libpq-15.12-clang-aarch64.tar.xz"
   } else if(grepl("clang", Sys.getenv('R_COMPILED_BY'))){
-    "https://github.com/r-windows/bundles/releases/download/libpq-15.11/libpq-15.11-clang-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/libpq-15.12/libpq-15.12-clang-x86_64.tar.xz"
   }  else if(getRversion() >= "4.2") {
-    "https://github.com/r-windows/bundles/releases/download/libpq-15.11/libpq-15.11-ucrt-x86_64.tar.xz"
+    "https://github.com/r-windows/bundles/releases/download/libpq-15.12/libpq-15.12-ucrt-x86_64.tar.xz"
   } else {
     "https://github.com/rwinlib/libpq/archive/v13.2.0.tar.gz"
   }
